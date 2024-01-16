c03bf430dc8c94449fecaab8b70f554e6af05d79
Sami Männistö <sami.mannisto1@gmail.com>
2024-01-16 17:18:24 +0200

    Task 1. Tokenizer function base.

--- src/compiler/tokenizer.py
+++ src/compiler/tokenizer.py
@@ -1,6 +1,18 @@
 import re
 
 def tokenize(source_code: str) -> list[str]:
-    # TODO
     tokens = []
+
+    keywords = re.compile(r'\b(while|for|if|else|elif)\b')
+    variables = re.compile(
+        r'\b_[a-zA-Z][A-Za-z0-9_]*\b|\b[a-zA-Z][A-Za-z0-9_]*\b')
+    int_literals = re.compile(r'\-?\b[1-9]+\b|\b0\b')
+
+    matches = re.finditer(
+        f'{keywords.pattern}|{variables.pattern}|{int_literals.pattern}', source_code)
+
+    for match in matches:
+        token = match.group(0)
+        tokens.append(token)
+
     return tokens
