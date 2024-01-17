c1710df1e7d7e2531f73ea3ea44d6b7c53ddcedc
Sami Männistö <sami.mannisto1@gmail.com>
2024-01-17 13:05:26 +0200

    Better variables and cleaner code

--- src/compiler/tokenizer.py
+++ src/compiler/tokenizer.py
@@ -6,21 +6,24 @@ def tokenize(source_code: str) -> list[str]:
     identifiers = regex.compile(r'(?P<IDENTIFIER>_?[A-Za-z_]+[a-zA-Z0-9_]*)')
     int_literals = regex.compile(r'(?P<INT_LITERAL>\-?\b[1-9]+\b|\b0\b)')
     newline = regex.compile(r'(?P<NEWLINE>\n)')
-    skip = regex.compile(r'(?P<SKIP>[ \t]+)')
+    whitespace = regex.compile(r'(?P<WHITESPACE>[ \t]+)')
     exception = regex.compile(r'(?P<EXCEPT>.|//)')
 
     merged_regex = regex.compile("|".join(
-        [identifiers.pattern, int_literals.pattern, newline.pattern, skip.pattern, exception.pattern]))
+        [identifiers.pattern,
+         int_literals.pattern,
+         newline.pattern,
+         whitespace.pattern,
+         exception.pattern]
+    ))
 
-    matches = merged_regex.finditer(source_code)
-
-    for match in matches:
+    for match in merged_regex.finditer(source_code):
         token_type = match.lastgroup
         value = match.group()
 
         if token_type == "NEWLINE":
             continue
-        elif token_type == "SKIP":
+        elif token_type == "WHITESPACE":
             continue
         elif token_type == "EXCEPT":
             raise RuntimeError(f"Invalid character: '{value}'")
