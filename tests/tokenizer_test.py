cbb39ca2324e9d2787de4df9e64dd9ae4c0d5d30
Sami Männistö <sami.mannisto1@gmail.com>
2024-01-18 09:13:04 +0200

    Cleaned code

--- tests/tokenizer_test.py
+++ tests/tokenizer_test.py
@@ -7,19 +7,19 @@ class TestTokenizeFunction(unittest.TestCase):
         source_code = "If x is not 10 then return 1"
 
         expected_tokens = [
-            Token(L(0, 0), 'IDENTIFIER', 'If'),
-            Token(L(0, 3), 'IDENTIFIER', 'x'),
-            Token(L(0, 5), 'IDENTIFIER', 'is'),
-            Token(L(0, 8), 'IDENTIFIER', 'not'),
-            Token(L(0, 12), 'INT_LITERAL', '10'),
-            Token(L(0, 16), 'IDENTIFIER', 'then'),
-            Token(L(0, 22), 'IDENTIFIER', 'return'),
-            Token(L(0, 29), 'INT_LITERAL', '1'),
+            Token(L(0, 0), "IDENTIFIER", "If"),
+            Token(L(0, 3), "IDENTIFIER", "x"),
+            Token(L(0, 5), "IDENTIFIER", "is"),
+            Token(L(0, 8), "IDENTIFIER", "not"),
+            Token(L(0, 12), "INT_LITERAL", "10"),
+            Token(L(0, 15), "IDENTIFIER", "then"),
+            Token(L(0, 20), "IDENTIFIER", "return"),
+            Token(L(0, 27), "INT_LITERAL", "1"),
         ]
 
         actual_tokens = tokenize(source_code)
 
         for expected, actual in zip(expected_tokens, actual_tokens):
-            self.assertTrue(expected.loc, actual.loc)
+            self.assertEqual(expected.loc, actual.loc)
             self.assertEqual(expected.type, actual.type)
             self.assertEqual(expected.text, actual.text)
