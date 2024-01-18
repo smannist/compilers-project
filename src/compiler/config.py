debf02022741a277f13305fa7498977620bad80d
Sami Männistö <sami.mannisto1@gmail.com>
2024-01-18 07:46:10 +0200

    Disallow negative integers

--- src/compiler/config.py
+++ src/compiler/config.py
@@ -3,7 +3,7 @@
 
 TOKEN_DICT = {
     "IDENTIFIER": r'(?P<IDENTIFIER>_?[A-Za-z_]+[a-zA-Z0-9_]*)',
-    "INT_LITERAL": r'(?P<INT_LITERAL>\-?\b[1-9][0-9]*\b|\b0\b)',
+    "INT_LITERAL": r'(?P<INT_LITERAL>\b[1-9][0-9]*\b|\b0\b)',
     "NEWLINE": r'(?P<NEWLINE>\n)',
     "WHITESPACE": r'(?P<WHITESPACE>[ \t]+)',
     "EXCEPT": r'(?P<EXCEPT>.)',
