b545cbc76053efdda60d7208482c3230ba9db767
Sami Männistö <sami.mannisto1@gmail.com>
2024-02-15 20:15:12 +0200

    Add base for ast

---
+++ src/compiler/ast.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+
+@dataclass
+class Expression:
+    """Base class for AST nodes representing expressions."""
+
+@dataclass
+class Literal(Expression):
+    value: int | bool | None
+
+@dataclass
+class Identifier(Expression):
+    name: str
+
+@dataclass
+class BinaryOp(Expression):
+    """AST node for a binary operation like `A + B`"""
+    left: Expression
+    op: str
+    right: Expression
